03e383bbce80209dc56e302e1d020d4cb4b51b5d
曲元涛 <yuantaojoy@126.com>
2020-04-02 20:05:06 +0800

    Initial commit

---
+++ src/main/java/com/geek/foobar/FoobarApplication.java
@@ -0,0 +1,13 @@
+package com.geek.foobar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoobarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoobarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geek/foobar/FoobarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geek.foobar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoobarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
