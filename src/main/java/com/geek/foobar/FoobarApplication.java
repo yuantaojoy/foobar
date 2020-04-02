fdbc4deb5cbedcc0d7fcae81d27eb03903176438
曲元涛 <yuantaojoy@126.com>
2020-04-02 20:20:37 +0800

    hello Foobar

--- src/main/java/com/geek/foobar/FoobarApplication.java
+++ src/main/java/com/geek/foobar/FoobarApplication.java
@@ -1,13 +1,24 @@
 package com.geek.foobar;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * FooBar !!
+ *
+ * @author 曲元涛
+ * @date 2020/4/2 20:12
+ */
 @SpringBootApplication
-public class FoobarApplication {
+public class FoobarApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoobarApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello FooBar");
+	}
 }
